143c9e897de6f26d279c3381f4b529b19a329e6f
rui-wang-8 <67623023+rui-wang-8@users.noreply.github.com>
2021-03-19 16:19:04 +0800

    Add files via upload

---
+++ target/h/m2IfLib.h
@@ -0,0 +1,26 @@
+/* m2IfLib.h - VxWorks MIB-II interface to SNMP Agent */
+
+/* Copyright 1984-1993 Wind River Systems, Inc. */
+#include "copyright_wrs.h"
+
+/*
+modification history
+--------------------
+01f,11feb00,ann written for RFC 2233 implementation
+*/
+
+#ifndef __INCm2IfLibh
+#define __INCm2IfLibh
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "m2Lib.h"
+#include "end.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __INCm2Libh */
